460c419d00c21b9a70476469114fa0a66fa306e4
roey <roeybh@gmail.com>
2019-10-21 18:21:31 +0300

    create tables as required. Basic test with provided dataset

--- database/movie_recommendation_database.sql
+++ database/movie_recommendation_database.sql
@@ -1,37 +1,65 @@
-/* how to creat a table with one entry */
-create table movie (
-     ID int,
-     Genere varchar(255),
-     TitleName varchar(255),
-     Rating int,
-     primary key (ID)
-);
 
-/* insert value to the table*/
-insert into movie(ID, Genere, TitleName, Rating)
-values (1,'kids', 'down', 5)
+create table users (
+     userid int,
+     name text,
+     primary key (userid)
+)
 
-insert into movie(ID, Genere, TitleName, Rating)
-values (2,'kids', 'down_up', 5)
+create table movies (
+     movieid int,
+     title text,
+     primary key (movieid)
+)
+
+create table taginfo (
+     tagid int,
+     content text,
+     primary key (tagid)
+)
+
+create table geners (
+     generid int,
+     name text,
+     primary key (generid)
+)
+
+create table ratings (
+     userid int,
+     movieid int,
+     rating numeric check (rating >= 0.0 and rating <= 5.0),
+     timestamp bigint,
+     foreign key(userid) references users,
+     foreign key(movieid) references movies,
+     primary key(userid,movieid)
+)
+
+create table tags (
+     userid int,
+     movieid int,
+     tagid int,
+     timestamp bigint,
+     foreign key (userid) references users,
+     foreign key (movieid) references movies,
+     foreign key (tagid) references taginfo
+)
+
+create table hasgenre (
+     movieid int,
+     generid int,
+     foreign key (movieid) references movies,
+     foreign key (generid) references geners
+)
+
+copy users from '/Users/roeybenhayun/Projects/ASU/Coursera-ASU-Database/course1/assignment1/exampleinput/users.dat' delimiter '%'
+copy movies from '/Users/roeybenhayun/Projects/ASU/Coursera-ASU-Database/course1/assignment1/exampleinput/movies.dat' delimiter '%'
+copy taginfo from '/Users/roeybenhayun/Projects/ASU/Coursera-ASU-Database/course1/assignment1/exampleinput/taginfo.dat' delimiter '%'
+copy geners from '/Users/roeybenhayun/Projects/ASU/Coursera-ASU-Database/course1/assignment1/exampleinput/genres.dat' delimiter '%'
+copy ratings from '/Users/roeybenhayun/Projects/ASU/Coursera-ASU-Database/course1/assignment1/exampleinput/ratings.dat' delimiter '%'
+copy tags from '/Users/roeybenhayun/Projects/ASU/Coursera-ASU-Database/course1/assignment1/exampleinput/tags.dat' delimiter '%'
+copy hasgenre from '/Users/roeybenhayun/Projects/ASU/Coursera-ASU-Database/course1/assignment1/exampleinput/hasagenre.dat' delimiter '%'
 
 
-insert into movie(ID, Genere, TitleName, Rating)
-values (3,'comedy', 'up', 5)
 
 
-insert into movie(ID, Genere, TitleName, Rating)
-values (4,'action', 'unbroaken', 2)
-
-
-insert into movie(ID, Genere, TitleName, Rating)
-values (5,'action', 'in the river', 4)
-
-/* select all from the table */
-select * from movie
-
-/* get value from the table according to some rule*/
-select * from movie
-where rating > 3
-
-/* delete a table*/
+/*select * from movie*/
 /*drop table movie*/
