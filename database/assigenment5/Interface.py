0a5b332fad202930d94a67fb727a893476f6b91a
roey <roeybh@gmail.com>
2019-11-13 18:24:14 +0200

    change algo impl.

--- database/assigenment5/Interface.py
+++ database/assigenment5/Interface.py
@@ -13,17 +13,49 @@ import threading
 l = threading.Lock()
 index_ = []
 value_ = []
-
-def thread_function(unsorted_list, start_index, batch_size):
-    # ok not under mutex. Each thread is working on different part of the unsorted array
+temp_list = []
+def thread_function(unsorted_list, start_index, batch_size,column_index):
+    
     sublist = unsorted_list[start_index:start_index+batch_size]
     
     l.acquire()
-    sublist_min = min(sublist)
-    min_index = sublist.index(sublist_min)
-    value_.append(sublist_min)
-    index_.append(min_index)
+    v_min = sublist[0][column_index]
+    v_min_index = 0
+    for i in range(1,len(sublist)):
+        v = sublist[i][column_index]
+        if(v < v_min):
+            v_min = v
+            v_min_index = i
+    
+    value_.append(sublist[v_min_index])
+    index_.append(start_index+v_min_index)
     l.release()
+
+
+def foo_function(unsorted_list, start_index, batch_size):
+    print "IN FOO"
+    print "Start Index = ", start_index
+    print "Batch Size = ", batch_size
+    sublist = unsorted_list[start_index:start_index+10]
+
+    #for i in range(0,10):
+    #    print sublist[i][0]
+    
+    #print (sublist[:][:])
+    #sublist_min = min(sublist)
+    v_min = sublist[0][2]
+    v_min_index = 0
+    for i in range(1,len(sublist)):
+        v = sublist[i][2]
+        if(v < v_min):
+            v_min = v
+            v_min_index = i
+    
+    print "MIN = ", v_min
+    print "V_MIN Index = ", v_min_index
+    print "TUPLE = ", sublist[v_min_index]
+
+
 
 # Donot close the connection inside this file i.e. do not perform openconnection.close()
 def ParallelSort (InputTable, SortingColumnName, OutputTable, openconnection):
@@ -38,31 +70,49 @@ def ParallelSort (InputTable, SortingColumnName, OutputTable, openconnection):
     unsorted_list = []
     # get the size of the list to be sorted
     cursor = openconnection.cursor()
-    command = (""" SELECT SortingColumnName from InputTable """)
-    query = str.replace(command,'SortingColumnName', SortingColumnName)
-    query = str.replace(query,'InputTable', InputTable)
+    #command = (""" SELECT SortingColumnName from InputTable """)
+    #query = str.replace(command,'SortingColumnName', SortingColumnName)
+    # get the table
+    #cursor.execute("SELECT * FROM ratings LIMIT 0")
+    #colnames = [desc[0] for desc in cursor.description]
+    #print colnames
+    #print SortingColumnName
+    #return
+    command = (""" SELECT * from InputTable """)
+    query = str.replace(command,'InputTable', InputTable)
     print(query)
     cursor.execute(query)
     rows = cursor.fetchall()
 
-    for row in rows:
-        val1 = row[0]
+    #print "ROWSTYPE = ",type(rows), "SIZE = ", len(rows)
+    #return
+    #for row in rows:
+    #    val1 = row[0]
         #print (val1)
-        unsorted_list.append(val1)
+    #    unsorted_list.append(val1)
 
+    # all the rows
+    unsorted_list = rows
     orig_size = len(unsorted_list)
     batch_size = len(unsorted_list)/number_of_workers
     leftover = len(unsorted_list)%number_of_workers
     #print("Batch size = ",batch_size)
     #print("List size", len(unsorted_list))
 
+    # Just to figure out how to sort nested list
+    #foo_function(unsorted_list,0,batch_size)
+
+
+
+
+    #return
     #exit
     sort = True
     threads = list()
 
     while(sort==True):
         # give the start index and size for each thread
-        print("Start...")
+        #print("Start...")
         for index in range(0,number_of_workers):
             length = batch_size
             # the last thread should work on the rest of the list which is the batch size + leftover
@@ -70,47 +120,51 @@ def ParallelSort (InputTable, SortingColumnName, OutputTable, openconnection):
                 length = batch_size+leftover  
 
             start_index = index * batch_size
-            print("Start Index = ", start_index)
-            print("Length= ", length)
-            x = threading.Thread(target=thread_function, args=(unsorted_list, start_index, length))
+            #print("Start Index = ", start_index)
+            #print("Length= ", length)
+            x = threading.Thread(target=thread_function, args=(unsorted_list, start_index, length,2))
             threads.append(x)
             x.start()
         
         for index, thread in enumerate(threads):
             thread.join()
 
-        #print("Joined all threads. Do some post processing here...")
-        #for i in range(0, len(index_)):
-        #    print("Index = ", index_[i])
-        #    print("Value = ", value_[i])
-        
         # get mean value, get the index. delete this item from the list and save the tuple to file
-        m_l = locate_min(value_)
-        current_min = m_l[0]
-        current_min_index_list = m_l[1]
-        print("M-L = ",m_l)
+        
+        #for i in range(0,number_of_workers):
+        #print(value_)
+        #print(index_)
 
-        for i in range(0,len(current_min_index_list)):
-            val = index_[current_min_index_list[i]]
-            s1 = str(current_min)+ ','+str(val)
-            f_out.write(s1)
-            f_out.write('\n')
-            # remove the items from the list
-            del unsorted_list[val]
-            orig_size = orig_size - 1
-            
+        min_value = value_[0][2]
+        min_index = 0
+        for i in range (1,number_of_workers):
+            temp_min = value_[i][2]
+            if temp_min < min_value:
+                min_value = temp_min
+                min_index = i
+
+        #print value_[min_index]
+        f_out.write(str(value_[min_index]))
+        f_out.write('\n')
+
+
+        
+        delete_index = index_[min_index]
+        del unsorted_list[delete_index]
+
+        print("batch_size = ", batch_size)
         batch_size = len(unsorted_list)/number_of_workers
         leftover = len(unsorted_list)%number_of_workers
 
 
-        print("Number of minimas = ", len(current_min_index_list))
-        print("Batch Size = ", batch_size)
-        print("Leftovers = ", leftover)
-        print("Current size = ", orig_size)
-
         # loop break condition
-        if (orig_size <= 5):
-            print("BREAK LOOP.... size = ", orig_size)
+        if (len(unsorted_list) <= 5):
+            print unsorted_list
+            print index_
+            print("BREAK LOOP.... size = ", len(unsorted_list))
+            print("LEFTOVER... size = ", leftover)
+            last = Sort(unsorted_list,2)
+            print (last)
             sort = False
 
         # get the tuple and delete the list
@@ -122,10 +176,17 @@ def ParallelSort (InputTable, SortingColumnName, OutputTable, openconnection):
     f_out.close()
         
 
-def locate_min(a):
-    smallest = min(a)
-    return smallest, [index for index, element in enumerate(a) 
-                      if smallest == element]
+
+
+def Sort(sub_li,column):   
+    # reverse = None (Sorts in Ascending order) 
+    # key is set to sort using second element of  
+    # sublist lambda has been used 
+    return(sorted(sub_li, key = lambda x: x[column]))     
+  
+
+
+
 
 def ParallelJoin (InputTable1, InputTable2, Table1JoinColumn, Table2JoinColumn, OutputTable, openconnection):
     #Implement ParallelJoin Here.
