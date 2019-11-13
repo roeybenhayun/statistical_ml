1a8f3f41af942aaf58df025a12df41d049af0c3c
roey <roeybh@gmail.com>
2019-11-13 18:29:26 +0200

    some cleanup

--- database/assigenment5/Interface.py
+++ database/assigenment5/Interface.py
@@ -13,12 +13,13 @@ import threading
 l = threading.Lock()
 index_ = []
 value_ = []
-temp_list = []
+
+
 def thread_function(unsorted_list, start_index, batch_size,column_index):
     
-    sublist = unsorted_list[start_index:start_index+batch_size]
-    
+    sublist = unsorted_list[start_index:start_index+batch_size]    
     l.acquire()
+    
     v_min = sublist[0][column_index]
     v_min_index = 0
     for i in range(1,len(sublist)):
@@ -32,42 +33,22 @@ def thread_function(unsorted_list, start_index, batch_size,column_index):
     l.release()
 
 
-def foo_function(unsorted_list, start_index, batch_size):
-    print "IN FOO"
-    print "Start Index = ", start_index
-    print "Batch Size = ", batch_size
-    sublist = unsorted_list[start_index:start_index+10]
-
-    #for i in range(0,10):
-    #    print sublist[i][0]
-    
-    #print (sublist[:][:])
-    #sublist_min = min(sublist)
-    v_min = sublist[0][2]
-    v_min_index = 0
-    for i in range(1,len(sublist)):
-        v = sublist[i][2]
-        if(v < v_min):
-            v_min = v
-            v_min_index = i
-    
-    print "MIN = ", v_min
-    print "V_MIN Index = ", v_min_index
-    print "TUPLE = ", sublist[v_min_index]
-
-
-
 # Donot close the connection inside this file i.e. do not perform openconnection.close()
 def ParallelSort (InputTable, SortingColumnName, OutputTable, openconnection):
+    print("Start Parallel Sort")
     global value_
     global index_
-    print("Start Parallel Sort")
 
-    point__query_file_name='parallel_sort_results.txt'
-    f_out = open(point__query_file_name, 'w')
+
+    save_to_file = False
+
+    if save_to_file == True:
+        point__query_file_name='parallel_sort_results.txt'
+        f_out = open(point__query_file_name, 'w')
     
     number_of_workers = 5
     unsorted_list = []
+
     # get the size of the list to be sorted
     cursor = openconnection.cursor()
     #command = (""" SELECT SortingColumnName from InputTable """)
@@ -78,50 +59,29 @@ def ParallelSort (InputTable, SortingColumnName, OutputTable, openconnection):
     #print colnames
     #print SortingColumnName
     #return
+    
     command = (""" SELECT * from InputTable """)
     query = str.replace(command,'InputTable', InputTable)
     print(query)
     cursor.execute(query)
     rows = cursor.fetchall()
 
-    #print "ROWSTYPE = ",type(rows), "SIZE = ", len(rows)
-    #return
-    #for row in rows:
-    #    val1 = row[0]
-        #print (val1)
-    #    unsorted_list.append(val1)
-
     # all the rows
     unsorted_list = rows
-    orig_size = len(unsorted_list)
     batch_size = len(unsorted_list)/number_of_workers
     leftover = len(unsorted_list)%number_of_workers
-    #print("Batch size = ",batch_size)
-    #print("List size", len(unsorted_list))
 
-    # Just to figure out how to sort nested list
-    #foo_function(unsorted_list,0,batch_size)
-
-
-
-
-    #return
-    #exit
     sort = True
     threads = list()
 
     while(sort==True):
-        # give the start index and size for each thread
-        #print("Start...")
         for index in range(0,number_of_workers):
             length = batch_size
-            # the last thread should work on the rest of the list which is the batch size + leftover
+            
             if (index == number_of_workers-1):
                 length = batch_size+leftover  
 
-            start_index = index * batch_size
-            #print("Start Index = ", start_index)
-            #print("Length= ", length)
+            start_index = index * batch_size                        
             x = threading.Thread(target=thread_function, args=(unsorted_list, start_index, length,2))
             threads.append(x)
             x.start()
@@ -129,11 +89,6 @@ def ParallelSort (InputTable, SortingColumnName, OutputTable, openconnection):
         for index, thread in enumerate(threads):
             thread.join()
 
-        # get mean value, get the index. delete this item from the list and save the tuple to file
-        
-        #for i in range(0,number_of_workers):
-        #print(value_)
-        #print(index_)
 
         min_value = value_[0][2]
         min_index = 0
@@ -143,12 +98,11 @@ def ParallelSort (InputTable, SortingColumnName, OutputTable, openconnection):
                 min_value = temp_min
                 min_index = i
 
-        #print value_[min_index]
-        f_out.write(str(value_[min_index]))
-        f_out.write('\n')
 
+        if save_to_file == True:
+            f_out.write(str(value_[min_index]))
+            f_out.write('\n')
 
-        
         delete_index = index_[min_index]
         del unsorted_list[delete_index]
 
@@ -166,14 +120,12 @@ def ParallelSort (InputTable, SortingColumnName, OutputTable, openconnection):
             last = Sort(unsorted_list,2)
             print (last)
             sort = False
-
-        # get the tuple and delete the list
-        #print(m_l)
+        
         index_ = []
         value_ = []
-        #time.sleep(3)
-
-    f_out.close()
+    
+    if save_to_file == True:      
+        f_out.close()
         
 
 
